0869098553c9ccb4af50b04ba79e70b4791cbdd0
Alexleadr <alex.leadr@gmail.com>
2019-09-17 23:10:04 +0900

    Update order.php

--- order.php
+++ order.php
@@ -1,6 +1,6 @@
 <?php
 include_once(__DIR__ . DIRECTORY_SEPARATOR . 'TD.php');
-include_once(__DIT__ . DIRECTORY_SEPARATOR . 'helpers.php');
+include_once(__DIR__ . DIRECTORY_SEPARATOR . 'helpers.php');
 
 $userId = 0;
 $apiKey = 'asfsa924.............fasf8902';
